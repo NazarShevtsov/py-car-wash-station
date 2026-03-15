f13f77ab0b084e416be75a43f51a2dbd97a601c7
Nazar Shevtsov <nazarshevtsovboyron@gmail.com>
2026-03-15 20:34:11 +0100

    CarWashStation edited names of params

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 class Car:
-    def __init__(self, comfort_cl: int, clean_mark: int, brand: str) -> None:
-        self.comfort_class = comfort_cl
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, dist: float, power: int, rate: float, num: int) -> None:
-        self.distance_from_city_center = dist
-        self.clean_power = power
-        self.average_rating = rate
-        self.count_of_ratings = num
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
         diff = int(self.clean_power) - int(car.clean_mark)
